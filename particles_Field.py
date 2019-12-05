8ca1967cb257b54c1cf99e90e6ddd0724ffceffc
corey33p <corey33p@gmail.com>
2019-12-05 14:52:32 -0600

    Add files via upload

--- particles_Field.py
+++ particles_Field.py
@@ -6,11 +6,17 @@ class Field:
         self.population = population
         self.coords = np.random.random((self.population,2))
         self.mass = np.random.random((self.population,1))
-    def distance_from(self,coords):
-        deltas = coords - self.coords
-        x_coords = coords[...,0].reshape(self.population,1)
-        y_coords = coords[...,1].reshape(self.population,1)
+    def get_distances(self,deltas):
+        x_coords = deltas[...,0].reshape(self.population,1)
+        y_coords = deltas[...,1].reshape(self.population,1)
         distances = (x_coords**2 + y_coords**2)**.5
         return distances
     def move(self):
-        gravitational_energy = 
+        mass_products = self.mass.dot(self.mass.T)
+        coords_repeat = self.coords.reshape(self.population,2,1)
+        coords_repeat = np.repeat(coords_repeat,self.population,axis=3)
+        # self.coords may need reshaped here
+        coordinate_deltas = self.coords - coords_repeat
+        coordinate_deltas = coordinate_deltas.reshape(self.population**2,2)
+        distances = self.get_distances(coordinate_deltas)
+        
